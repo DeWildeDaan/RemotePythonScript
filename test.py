b223a554ba2680328f85c0208d61a5ce7720c34b
DeWildeDaan <daan@LAPTOP-SCHOOL.localdomain>
2023-12-23 16:08:29 +0100

    Added execute scripts

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
 print("Hello World")
+
+
+
+# Execute the script:
+# Passes the code to python as a string and executes it
+# python3 -c "$(wget -q -O - https://raw.githubusercontent.com/DeWildeDaan/remotepythontest/main/test.py)"
+
+# Downloads the script, exectutes it and removes it
+# wget https://raw.githubusercontent.com/DeWildeDaan/remotepythontest/main/test.py && (python3 test.py; rm test.py)
